d0fe345fde5b6100ac4ca3ef5fc366d09d0a02fe
Ness <sampaio_88@hotmail.com>
2026-04-13 09:47:39 +0100

    initial solution setup

---
+++ csharp-mars-rover/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_mars_rover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
