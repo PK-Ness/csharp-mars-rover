5eb37cae5d015bc36c9d17dc56f5c1d4564ec7be
Ness <sampaio_88@hotmail.com>
2026-04-13 10:14:21 +0100

    FEAT: Create Enum Direction

--- MarsRover.Tests/UnitTest1.cs
+++ MarsRover.Tests/UnitTest1.cs
@@ -8,9 +8,9 @@ namespace MarsRover.Tests
         }
 
         [Test]
-        public void Test1()
+        public void Successfully_Runs_A_Test()
         {
-            Assert.Pass();
+            Assert.That(true, Is.True);
         }
     }
 }
--- MarsRover.Tests/obj/Debug/net8.0/MarsRover.Tests.AssemblyInfo.cs
+++ MarsRover.Tests/obj/Debug/net8.0/MarsRover.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarsRover.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0fe345fde5b6100ac4ca3ef5fc366d09d0a02fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarsRover.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarsRover.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
